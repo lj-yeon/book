a10889deb1753169516ddb3ac556f53d4f262ab4
dlwoduszz <lj_yeon_@naver.com>
2023-02-20 19:15:19 +0900

    23.02.20

--- js/sub.js
+++ js/sub.js
@@ -14,27 +14,53 @@ $(document).ready(function(){
     },function(){
         $(this).find('.left_right').children().hide();
     })
-    var count=0;
+    var vcount=0;
+    if(vcount<$('.img_boxy li').length){
+        $('.img_boxy li:nth-last-of-type(1)').css({'display':'none'})
+    };
     $('.img_box').find('.right_btn').click(function(){
-        count++;
-        console.log(count)
-        if(count>=$('.img_boxy li').length){
-            count=$('.img_boxy li').length-1;
+        vcount++;
+        vconsole.log(count)
+        if(vcount>=$('.img_boxy li').length-1){
+            vcount=$('.img_boxy li').length-1
             $(this).css({'opacity':'0.5'});
         }
-        $('.img_boxy li').eq(count).fadeIn().siblings().fadeOut();
+        $('.img_boxy li').eq(vcount).fadeIn().siblings().fadeOut();
         $(this).siblings().css({'opacity':'1'});
-        $('.img_btn').find('span').eq(0).html(Number(count+1))
+        $('.img_btn').find('span').eq(0).html(Number(vcount+1))
 
     })
     $('.img_box').find('.left_btn').click(function(){
-        count--;
-        if(count<0){
-            count=0;
+        vcount--;
+        if(vcount<=0){
+            vcount=0;
             $(this).css({'opacity':'0.5'});
         }
-        $('.img_boxy li').eq(count).fadeIn().siblings().fadeOut();
+        $('.img_boxy li').eq(vcount).fadeIn().siblings().fadeOut();
         $(this).siblings().css({'opacity':'1'});
-        $('.img_btn').find('span').eq(0).html(Number(count+1))
+        $('.img_btn').find('span').eq(0).html(Number(vcount+1))
     });
+
+    //북카드
+    $('.bookcard').hover(function(){
+        $(this).find('.left_right').children().fadeIn(300);
+    },function(){
+        $(this).find('.left_right').children().fadeOut(300);
+    })
+    
+    var bookmove= $('.bookcard button').parent().siblings('.substance').find('ul');
+
+    $('.bookcard').find('.left_btn').click(function(){
+        bookmove.stop().animate({'margin-left':'-51%'},function(){
+            $(this).children().first().appendTo(this);
+            $(this).css({'margin-left':'0'});
+       });
+    })
+    $('.bookcard').find('.right_btn').click(function(){
+        bookmove.css({'margin-left':'-51%'});
+        bookmove.children().last().prependTo(bookmove);
+        bookmove.stop().animate({'margin-left':'0%'});
+    })
+
+
 })
